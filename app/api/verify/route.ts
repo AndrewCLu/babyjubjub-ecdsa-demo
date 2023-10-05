8ecdb1c9ccc82a0567bc353b14ae5b16d2cd020d
AndrewCLu <andrewclu98@gmail.com>
2023-10-05 00:36:25 -0400

    update to 1.0.4

--- app/api/verify/route.ts
+++ app/api/verify/route.ts
@@ -1,8 +1,12 @@
+const log = require("why-is-node-running");
 import { NextResponse } from "next/server";
 import { verifyMembership } from "babyjubjub-ecdsa";
 import path from "path";
 
 export async function POST(request: Request) {
+  // setTimeout(function () {
+  //   log(); // logs out active handles that are keeping node running
+  // }, 5000);
   try {
     const bodyArrayBuffer = await request.arrayBuffer();
     const bodyString = new TextDecoder().decode(bodyArrayBuffer);
